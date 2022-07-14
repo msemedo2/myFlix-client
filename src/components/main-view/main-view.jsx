28a09a863e4e021dfb4367f30d221f1ba4d1003c
unknown <miguel.semedo.2@outlook.com>
2022-07-14 16:15:15 +0100

    import login and registration

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,50 +1,68 @@
 import React from 'react';
 import axios from 'axios';
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { LoginView } from '../login-view/login-view';
+import { RegistrationView } from '../registration-view/registration-view';
 
 export class MainView extends React.Component {
 	constructor() {
 		super();
+		// Initial state is set to null
 		this.state = {
-			movies: [
-				{
-					_id: 1,
-					Title: 'The Lord of the Rings: The Fellowship of the Ring',
-					Description:
-						'A meek Hobbit from the land of the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.',
-					ImagePath:
-						'https://upload.wikimedia.org/wikipedia/pt/3/38/Lord_of_the_Rings_Fellowship_of_the_Ring.jpg',
-				},
-				{
-					_id: 2,
-					Title: 'The Lord of the Rings: The Two Towers',
-					Description:
-						'While Frodo and Sam edge closer to Mordor with the help of the shifty Gollum, the divided fellowship makes a stand against Saurons new ally, Saruman, and his hordes of Isengard.',
-					ImagePath:
-						'https://m.media-amazon.com/images/M/MV5BZGMxZTdjZmYtMmE2Ni00ZTdkLWI5NTgtNjlmMjBiNzU2MmI5XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg',
-				},
-				{
-					_id: 3,
-					Title: 'The Lord of the Rings: The Return of the King',
-					Description:
-						'Gandalf and Aragorn lead the World of Men against Saurons army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.',
-					ImagePath:
-						'https://upload.wikimedia.org/wikipedia/en/b/be/The_Lord_of_the_Rings_-_The_Return_of_the_King_%282003%29.jpg',
-				},
-			],
+			movies: [],
+			selectedMovie: null,
+			user: null,
+			registered: null,
 		};
 	}
+
+	componentDidMount() {
+		axios
+			.get('https://mikeflix2.herokuapp.com/movies')
+			.then((response) => {
+				this.setState({
+					movies: response.data,
+				});
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
+
 	setSelectedMovie(newSelectedMovie) {
 		this.setState({
 			selectedMovie: newSelectedMovie,
 		});
 	}
-	render() {
-		const { movies, selectedMovie } = this.state;
 
-		if (movies.length === 0)
-			return <div className="main-view">The list is empty!</div>;
+	onLoggedIn(user) {
+		this.setState({
+			user,
+		});
+	}
+
+	onRegistration(registered) {
+		this.setState({
+			registered,
+		});
+	}
+
+	render() {
+		const { movies, selectedMovie, user, registered } = this.state;
+
+		if (!user)
+			return <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />;
+
+		if (!registered)
+			return (
+				<RegistrationView
+					onRegistration={(registered) => this.onRegistration(registered)}
+				/>
+			);
+
+		if (movies.length === 0) return <div className="main-view" />;
 
 		return (
 			<div className="main-view">
@@ -60,8 +78,8 @@ export class MainView extends React.Component {
 						<MovieCard
 							key={movie._id}
 							movie={movie}
-							onMovieClick={(movie) => {
-								this.setSelectedMovie(movie);
+							onMovieClick={(newSelectedMovie) => {
+								this.setSelectedMovie(newSelectedMovie);
 							}}
 						/>
 					))
