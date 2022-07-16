326fbb3d0de98d911d029448623d2f611826030e
unknown <miguel.semedo.2@outlook.com>
2022-07-16 20:52:45 +0100

    update registration component

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,65 +1,139 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import {
-	Button,
-	Card,
-	CardGroup,
-	Container,
-	Col,
-	Row,
-	Form,
-} from 'react-bootstrap';
+import axios from 'axios';
+
+import { Form, Button } from 'react-bootstrap';
+
+import './registration-view.scss';
+
 export function RegistrationView(props) {
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
 	const [email, setEmail] = useState('');
 	const [birthdate, setBirthDate] = useState('');
+	const [values, setValues] = useState({
+		usernameErr: '',
+		passwordErr: '',
+		emailErr: '',
+	});
+
+	const validate = () => {
+		let isReq = true;
+		if (!username) {
+			setValues({ ...values, usernameErr: 'Username Required' });
+			isReq = false;
+		} else if (username.length < 5) {
+			setValues({
+				...values,
+				usernameErr: 'Username must be 5 characters long',
+			});
+			isReq = false;
+		}
+		if (!password) {
+			setValues({ ...values, passwordErr: 'Password Required' });
+			isReq = false;
+		} else if (password.length < 6) {
+			setValues({
+				...values,
+				passwordErr: 'Password must be 6 characters long',
+			});
+			isReq = false;
+		}
+		if (!email) {
+			setValues({ ...values, emailErr: 'Email Required' });
+			isReq = false;
+		} else if (email.indexOf('@') === -1) {
+			setValues({ ...values, emailErr: 'Email is invalid' });
+			isReq = false;
+		}
+		return isReq;
+	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log(username, password, email, birthdate);
-		/* Send a request to the server for authentication */
-		/* then call props.onLoggedIn(username) */
-		props.onRegistration(username);
+		const isReq = validate();
+		if (isReq) {
+			axios
+				.post('https://mikeflix2.herokuapp.com/users', {
+					Username: username,
+					Password: password,
+					Email: email,
+					BirthDate: birthdate,
+				})
+				.then((response) => {
+					const data = response.data;
+					console.log(data);
+					alert('Registration successful, please login!');
+					window.open('/', '_self'); // the second argument is necessary so that the page will open in the current tab
+				})
+				.catch((response) => {
+					console.error(response);
+					alert('unable to register');
+				});
+		}
 	};
 
 	return (
 		<Form>
-			<Form.Group controlId="formUsername">
+			<h2 className="mb-3 mx-auto mt-5">MikeFlix Registration</h2>
+
+			<Form.Group className="mb-3 mx-auto mt-4" controlId="formUsername">
 				<Form.Label>Username:</Form.Label>
 				<Form.Control
 					type="text"
+					value={username}
 					onChange={(e) => setUsername(e.target.value)}
+					required
+					placeholder="Enter a username"
 				/>
+				{values.usernameErr && <p>{values.usernameErr}</p>}
 			</Form.Group>
-			<Form.Group controlId="formPassword">
+
+			<Form.Group className="mb-3 mx-auto mt-4">
 				<Form.Label>Password:</Form.Label>
 				<Form.Control
 					type="password"
+					value={password}
 					onChange={(e) => setPassword(e.target.value)}
+					required
+					minLength="6"
+					placeholder="Password must be 6 characters long"
 				/>
+				{values.passwordErr && <p>{values.passwordErr}</p>}
 			</Form.Group>
-			<Form.Group controlId="formEmail">
+
+			<Form.Group className="mb-3 mx-auto mt-4">
 				<Form.Label>Email:</Form.Label>
 				<Form.Control
 					type="email"
-					onChange={(e) => setPassword(e.target.value)}
+					value={email}
+					required
+					onChange={(e) => setEmail(e.target.value)}
 				/>
+				{values.emailErr && <p>{values.emailErr}</p>}
 			</Form.Group>
-			<Form.Group controlId="formBirthDate">
+
+			<Form.Group className="mb-3 mx-auto mt-4">
 				<Form.Label>BirthDate:</Form.Label>
 				<Form.Control
 					type="date"
-					onChange={(e) => setPassword(e.target.value)}
+					value={birthdate}
+					onChange={(e) => setBirthDate(e.target.value)}
 				/>
 			</Form.Group>
-			<Button variant="primary" type="submit" onClick={handleSubmit}>
-				Submit
+
+			<Button className="mt-4" type="submit" onClick={handleSubmit}>
+				Register
 			</Button>
 		</Form>
 	);
 }
 
 RegistrationView.propTypes = {
-	onRegistration: PropTypes.func.isRequired,
+	register: PropTypes.shape({
+		Username: PropTypes.string.isRequired,
+		Password: PropTypes.string.isRequired,
+		Email: PropTypes.string.isRequired,
+		BirthDate: PropTypes.string,
+	}),
 };
