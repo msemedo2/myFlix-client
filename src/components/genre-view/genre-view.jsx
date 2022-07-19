4dbedd122f573641e21741adf6f34abc9d737504
unknown <miguel.semedo.2@outlook.com>
2022-07-19 20:54:43 +0100

    update genre view

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Button, Container, Row, Col } from 'react-bootstrap';
+
 import './genre-view.scss';
 
 export class GenreView extends React.Component {
@@ -8,32 +8,29 @@ export class GenreView extends React.Component {
 		const { genre, onBackClick } = this.props;
 
 		return (
-			<Container className="genre-view">
+			<Container className="mt-3 genre-view">
 				<Row>
-					<Col className="label">Genre:</Col>
-					<Col className="value">{genre.Name}</Col>
+					<Col className="left-col">Genre: </Col>
+					<Col className="right-col">{genre?.Name}</Col>
 				</Row>
-				<Row>
-					<Col className="label">Description:</Col>
-					<Col className="value">{genre.Description}</Col>
+				<Row className="mt-3">
+					<Col className="left-col">Description: </Col>
+					<Col className="right-col">{genre?.Description}</Col>
 				</Row>
-
-				<Button
-					className="mt-4"
-					onClick={() => {
-						onBackClick(null);
-					}}
-				>
-					Back
-				</Button>
+				<Row className="mt-3">
+					<Col className="left-col"></Col>
+					<Col className="right-col">
+						<Button
+							className="d-block mt-3"
+							onClick={() => {
+								onBackClick(null);
+							}}
+						>
+							Back
+						</Button>
+					</Col>
+				</Row>
 			</Container>
 		);
 	}
 }
-
-GenreView.propTypes = {
-	genre: PropTypes.shape({
-		Name: PropTypes.string.isRequired,
-		Description: PropTypes.string.isRequired,
-	}).isRequired,
-};
